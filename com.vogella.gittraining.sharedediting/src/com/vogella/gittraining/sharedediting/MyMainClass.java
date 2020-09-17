4cd8a05f4df790a66c6ab1de7baf56e62cac528b
Ruth Devenish <Ruth.Devenish@fiduciagad.de>
2020-09-17 14:08:01 +0200

    Ruth Commit 3

--- com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/MyMainClass.java
+++ com.vogella.gittraining.sharedediting/src/com/vogella/gittraining/sharedediting/MyMainClass.java
@@ -19,7 +19,7 @@ public class MyMainClass {
 		System.out.println("someone");
 		System.out.println("Karl Heinz");
 		System.out.println("whatever");
-        System.out.println("Ruth Devenish");
+        System.out.println("Ruth Devenish mit Betonung auf Dev");
 	}
 
 }
